4664298cab91cf7a724b5396fbd0b1a9bbdbbb1e
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-21 13:14:37 +0530

    Update Button.tsx

--- source/parts/Button/Button.tsx
+++ source/parts/Button/Button.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./button.less";
-
 type Props = {
   children: any;
   onClick?: () => void;
 };
-
 export const Button = ({ children, onClick }: Props) => {
   return (
     <button className="button" onClick={onClick}>
