86280243ac1a516e3e70527bcc8369f0085b5bad
Yash Varma <71536409+yash509@users.noreply.github.com>
2024-02-21 13:14:21 +0530

    Update BoardContext.tsx

--- source/parts/Board/context/BoardContext.tsx
+++ source/parts/Board/context/BoardContext.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { tileCount as defaultTileCount } from "../index";
-
 export const BoardContext = React.createContext({
   containerWidth: 0,
   tileCount: defaultTileCount,
 });
-
 type Props = {
   containerWidth: number;
   tileCount: number;
   children: any;
 };
-
 export const BoardProvider = ({
   children,
   containerWidth = 0,
